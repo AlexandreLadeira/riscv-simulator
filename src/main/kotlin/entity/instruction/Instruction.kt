97c8249ff1c3332453c283e0f8a1a64def5ac60e
Alexandre Ladeira <alexandreladeira13@gmail.com>
2023-04-23 18:43:45 -0300

    Changes Instruction to abstract class

--- src/main/kotlin/entity/instruction/Instruction.kt
+++ src/main/kotlin/entity/instruction/Instruction.kt
@@ -2,7 +2,7 @@ package entity.instruction
 
 import entity.Simulator
 
-sealed class Instruction {
+abstract class Instruction {
     abstract fun execute(simulator: Simulator)
     abstract val disassembly: String
     open val cycleCost: Int = 1
